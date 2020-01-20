42852b29a02aef65754ca09d58f177ad85276bcd
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-20 12:05:55 +0700

    Tugas 20 Js

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+function tugas20() {
+  var isi = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log("index :",isi.match("are"));
+  console.log("index :",isi.match("words"));
+}
+tugas20();
